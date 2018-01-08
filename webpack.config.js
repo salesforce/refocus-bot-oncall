5394916b9f032ba3c397db900eb344a3bc18b3b0
Kris Foster <kfoster@salesforce.com>
2018-01-08 17:39:02 +0000

    some style and working

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,66 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin'); //creates index.html folder and puts it in dist folder
-var webpack = require('webpack');
-var ZipPlugin = require('zip-webpack-plugin');
-var env = process.env.NODE_ENV || 'dev';
-var url = require('./config.js')[env].refocusUrl;
+const path = require('path');
+// creates index.html folder and puts it in dist folder
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ZipPlugin = require('zip-webpack-plugin');
+const env = process.env.NODE_ENV || 'dev';
+const url = require('./config.js')[env].refocusUrl;
 const botName = require('./package.json').name;
 
-var config = {
+const config = {
+  entry: './web/index.js',
+  output: {
+    path: path.resolve(__dirname, './web/dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
 
-	entry: './web/index.js',
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: 'babel-loader?compact=true'}, //code transformer (if file is .js)
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.handlebars$/, loader: "handlebars-loader"},
+      {test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/, use: "url-loader?limit=100000"},
+    ]
+  },
 
-	output: {
-		path: path.resolve(__dirname, './web/dist'),
-		filename: 'index_bundle.js',
-		publicPath: '/'
-	},
+  node: {
+    fs: 'empty'
+  },
 
-	module: {
-		rules: [
-			{test: /\.(js|jsx)$/, use: 'babel-loader?compact=true'}, //code transformer (if file is .js)
-			{test: /\.css$/, use: ['style-loader', 'css-loader']},
-			{test: /\.handlebars$/, loader: "handlebars-loader"},
-			{test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/, use: "url-loader?limit=100000"},
-		]
-	},
+  devServer: {
+    historyApiFallback: true
+  },
 
-	node: {
-		fs: 'empty'
-	},
-
-	devServer: {
-		historyApiFallback: true
-	},
-
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'web/index.html',
-			url: url + '/v1/',
-			name: botName,
-		}),
-		new ZipPlugin({
-      		filename: 'bot.zip',
-      		include: [/\.js$/, /\.html$/],
-      		exclude: ['public']
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-				'API_TOKEN': JSON.stringify(process.env.API_TOKEN),
- 			}
- 		}),
-	]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'web/index.html',
+      url: url + '/v1/',
+      name: botName,
+    }),
+    new ZipPlugin({
+          filename: 'bot.zip',
+          include: [/\.js$/, /\.html$/],
+          exclude: ['public']
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'API_TOKEN': JSON.stringify(process.env.API_TOKEN),
+      }
+    }),
+  ]
 };
 
 if(process.env.NODE_ENV === 'production'){
-	config.plugins.push(
-		new webpack.DefinePlugin({ //allows us to set a property on process.env
-			'process.env': {
-				'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin()
-	);
+  config.plugins.push(
+    new webpack.DefinePlugin({ //allows us to set a property on process.env
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
 }
 
 module.exports = config;
