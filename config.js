5942a46a1e9f5bc69250d3a453f7a2d50ae4d33d
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2018-04-30 03:15:56 -0400

    Update Code

--- config.js
+++ config.js
@@ -1,35 +1,44 @@
+/* eslint no-process-env: 0 */
 /**
  * config.js
  * Config file for different deployments - dev, staging, production
  */
 
+const DEFAULT_PORT = 5000;
+
 module.exports = {
+  env: process.env.NODE_ENV || 'dev',
+  port: process.env.PORT || DEFAULT_PORT,
   dev: {
     refocusUrl: 'http://localhost:3000',
     sfdcUsername: process.env.SFDC_USERNAME,
     sfdcPassword: process.env.SFDC_PASSWORD,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
+    httpProxy: false,
     socketToken: process.env.SOCKET_TOKEN,
     pdToken: process.env.PD_TOKEN,
     pdSender: process.env.PD_SENDER,
   },
   staging: {
-    refocusUrl: 'http://refocus-staging.internal.salesforce.com',
+    refocusUrl: 'http://refocus-staging.herokuapp.com',
     sfdcUsername: process.env.SFDC_USERNAME,
     sfdcPassword: process.env.SFDC_PASSWORD,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
+    httpProxy: false,
     socketToken: process.env.SOCKET_TOKEN,
     pdToken: process.env.PD_TOKEN,
     pdSender: process.env.PD_SENDER,
   },
   sandbox: {
-    refocusUrl: 'https://refocus.hk.salesforce.com',
+    refocusUrl: 'https://refocus-sandbox.hk.salesforce.com',
     sfdcUsername: process.env.SFDC_USERNAME,
     sfdcPassword: process.env.SFDC_PASSWORD,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
+    httpProxy: process.env.http_proxy ||
+      'http://public0-proxy1-0-prd.data.sfdc.net:8080',
     socketToken: process.env.SOCKET_TOKEN,
     pdToken: process.env.PD_TOKEN,
     pdSender: process.env.PD_SENDER,
@@ -40,8 +49,11 @@ module.exports = {
     sfdcPassword: process.env.SFDC_PASSWORD,
     loginUrl: process.env.SFDC_URL,
     token: process.env.API_TOKEN,
+    httpProxy: process.env.http_proxy ||
+      'http://public0-proxy1-0-prd.data.sfdc.net:8080',
     socketToken: process.env.SOCKET_TOKEN,
     pdToken: process.env.PD_TOKEN,
     pdSender: process.env.PD_SENDER,
   },
 };
+
