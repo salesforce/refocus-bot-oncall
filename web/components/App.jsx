628ddd1e9f2f1ab10583c71f753074ac35199d10
kriscfoster <kfoster@kfoster-wsm1.internal.salesforce.com>
2018-09-10 11:09:17 +0100

    license

--- web/components/App.jsx
+++ web/components/App.jsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
--- web/components/ToastMessage.jsx
+++ web/components/ToastMessage.jsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
 const React=require('react');
 import PropTypes from 'prop-types';
 const TIMEOUT = 3000; // ms
