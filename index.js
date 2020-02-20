afd1158ebaf17467d46f504408edf14c28d97d94
Conal Cosgrove <conal.cosgrove@salesforce.com>
2020-02-20 14:37:08 +0000

    check that result body exists

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ function pdIncidentDetail(id) {
  */
 function getServices(offset) {
   return pdServices(offset).then((result) => {
+    if (!result.body) return {};
     // Feature Flag
     const resBody = USING_NEW_PD_BRIDGE ? result.body : result.body.services;
     services = services.concat(resBody);
