b5749b6614a27c0cac1e8927cd68751beda81640
Conal Cosgrove <cosgroco@tcd.ie>
2020-11-19 16:30:55 +0000

    update to utilize new participants sharedcontext

--- utils/autoPager.js
+++ utils/autoPager.js
@@ -31,7 +31,7 @@ const {
  */
 async function getIncidentsForRoom(roomId, botId) {
   const incidents = await bdk.getBotData(roomId, botId, 'onCallIncidents');
-  if (!incidents?.body?.[0].value) return [];
+  if (incidents?.body?.[0]?.value === undefined) return [];
   try {
     return JSON.parse(incidents.body[0].value)?.incidents;
   } catch (e) {
